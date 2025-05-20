22c1df09b18ab12bcc74169cfb8ba898fb18f096
Nicolas Beauvais <nicolas@bvs.email>
2025-05-20 14:33:45 +0200

    Bun switch

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/html; charset=utf-8")
-		fmt.Fprintf(w, "<!DOCTYPE html><html><head><title>Wizard Bunny</title></head><body><img src=\"https://wizbun.b-cdn.net/wizbun1.jpg\"/></body></html>")
+		fmt.Fprintf(w, "<!DOCTYPE html><html><head><title>Wizard Bunny</title></head><body><img src=\"https://wizbun.b-cdn.net/wizbun2.jpg\"/></body></html>")
 	})
 
 	http.ListenAndServe(":8080", nil)
