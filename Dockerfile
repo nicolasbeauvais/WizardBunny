dbed8323b58de7a9808987d4d81d9070f79d8cc4
Nicolas Beauvais <nicolas@bvs.email>
2025-05-20 12:28:26 +0200

    Use Bunny CDN for static files

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,6 @@ WORKDIR /
 
 # Copy the pre-built Go binary from GitHub Actions workflow
 COPY wizbun /usr/local/bin/wizbun
-COPY wizard-bunny.jpg /usr/local/bin/wizard-bunny.jpg
 
 # Ensure the binary is executable
 RUN chmod +x /usr/local/bin/wizbun
--- main.go
+++ main.go
@@ -8,12 +8,7 @@ import (
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/html; charset=utf-8")
-		fmt.Fprintf(w, "<!DOCTYPE html><html><head><title>Wizard Bunny</title></head><body><img src=\"/wizard-bunny.jpg\"/></body></html>")
-	})
-
-	http.HandleFunc("/wizard-bunny.jpg", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "image/jpeg")
-		http.ServeFile(w, r, "wizard-bunny.jpg")
+		fmt.Fprintf(w, "<!DOCTYPE html><html><head><title>Wizard Bunny</title></head><body><img src=\"https://wizbun.b-cdn.net/wizbun1.jpg\"/></body></html>")
 	})
 
 	http.ListenAndServe(":8080", nil)
